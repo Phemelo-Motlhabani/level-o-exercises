65d42035cc0e9906001a8080ea0136ad82791e3f
Phemelo-Motlhabani <skateboardhawkp@gmail.com>
2022-05-19 16:46:54 +0200

    fixed print error in task0_10.py

--- task0_10.py
+++ task0_10.py
@@ -1,7 +1,8 @@
 def common_characters(string_1, string_2):
-    for i in string_1:
-        if i in string_2:
-            print(i)
+    common_letters = ""
+    for letter in string_1:
+        if letter in string_2:
+            common_letters += letter + ","
         else:
             continue
-
+    print(f"Common characters: {common_letters}")
