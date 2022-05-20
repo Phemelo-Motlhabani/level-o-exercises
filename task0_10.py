39cb3172c3eb6b66a1eb0f10df68e0a73a32d0af
Phemelo-Motlhabani <skateboardhawkp@gmail.com>
2022-05-20 18:35:57 +0200

    fixed logical errors

--- task0_10.py
+++ task0_10.py
@@ -1,8 +1,14 @@
 def common_characters(string_1, string_2):
     common_letters = ""
-    for letter in string_1:
-        if letter in string_2:
-            common_letters += letter + ","
+    for letter in string_1.lower():
+        if letter in common_letters:
+            continue
+        elif letter in string_2.lower():
+            common_letters += letter
         else:
             continue
-    print(f"Common characters: {common_letters}")
+    sep = ","
+    print(f"Vowels: {sep.join(common_letters)}")
+
+
+common_characters("HOUSES", "computers")
