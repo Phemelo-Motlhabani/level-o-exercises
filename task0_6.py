3966d35d9e2344fcfdf429477cec7881be681445
Phemelo-Motlhabani <skateboardhawkp@gmail.com>
2022-05-20 18:13:00 +0200

    adjusted logic

--- task0_6.py
+++ task0_6.py
@@ -1,7 +1,11 @@
 def maximum(int_1, int_2, int_3):
-    if int_1 > int_2 and int_1 > int_3:
+    if int_1 >= int_2 and int_1 >= int_3:
         return int_1
-    if int_2 > int_1 and int_2 > int_3:
+    elif int_2 >= int_1 and int_2 >= int_3:
         return int_2
-    if int_3 > int_1 and int_3 > int_2:
+    elif int_3 >= int_1 and int_3 >= int_2:
         return int_3
+
+
+max_num = maximum(1, 2, 3)
+print(max_num)
