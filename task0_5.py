90b5e96d20b95c4254fa78f74cfdd1d390d4b103
Phemelo-Motlhabani <skateboardhawkp@gmail.com>
2022-05-20 18:07:32 +0200

    applied changes to code

--- task0_5.py
+++ task0_5.py
@@ -5,7 +5,11 @@ def area_of_triangle(side_1, side_2, side_3):
         print("Error: Triangle Inequality Theorem")
     if side_2 + side_3 <= side_1:
         print("Error: Triangle Inequality Theorem")
-    semi_p = 0.5 * (side_1 + side_2 + side_3)
-    area_squared = semi_p * (semi_p - side_1) * (semi_p - side_2) * (semi_p - side_3)
+    s = 0.5 * (side_1 + side_2 + side_3)
+    area_squared = s * (s - side_1) * (s - side_2) * (s - side_3)
     area = area_squared ** (1 / 2)
     return area
+
+
+triangle_area = area_of_triangle(3, 4, 5)
+print(triangle_area)
