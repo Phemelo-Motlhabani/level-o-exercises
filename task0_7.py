7786b60ce6407eeed60adb635554538d554be314
Phemelo-Motlhabani <skateboardhawkp@gmail.com>
2022-05-20 18:16:03 +0200

    added print

--- task0_7.py
+++ task0_7.py
@@ -6,3 +6,9 @@ def celsius_to_fahrenheit(degrees_celsius):
 def fahrenheit_to_celsius(degrees_fahrenheit):
     celsius = (degrees_fahrenheit - 32) * 5/9
     return celsius
+
+
+degrees_c = celsius_to_fahrenheit(36)
+print(degrees_c)
+degrees_f = fahrenheit_to_celsius(96.8)
+print(degrees_f)
