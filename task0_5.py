054bf8a0334c705c48f49f13f66758c6af771868
Phemelo-Motlhabani <skateboardhawkp@gmail.com>
2022-05-25 01:35:53 +0200

    descriptive variable name added

--- task0_5.py
+++ task0_5.py
@@ -5,8 +5,8 @@ def area_of_triangle(side_1, side_2, side_3):
         print("Error: Triangle Inequality Theorem")
     if side_2 + side_3 <= side_1:
         print("Error: Triangle Inequality Theorem")
-    s = 0.5 * (side_1 + side_2 + side_3)
-    area_squared = s * (s - side_1) * (s - side_2) * (s - side_3)
+    semi_perimeter = 0.5 * (side_1 + side_2 + side_3)
+    area_squared = semi_perimeter * (semi_perimeter - side_1) * (semi_perimeter - side_2) * (semi_perimeter - side_3)
     area = area_squared ** (1 / 2)
     return area
 
