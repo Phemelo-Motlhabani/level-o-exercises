a1a4b8f7616b677260246b05425b90bacccfe7da
Phemelo-Motlhabani <skateboardhawkp@gmail.com>
2022-08-03 18:53:18 +0200

    unnecessary if statements removed

--- task0_5.py
+++ task0_5.py
@@ -1,10 +1,4 @@
 def area_of_triangle(side_1, side_2, side_3):
-    if side_1 + side_2 <= side_3:
-        print("Error: Triangle Inequality Theorem")
-    if side_1 + side_3 <= side_2:
-        print("Error: Triangle Inequality Theorem")
-    if side_2 + side_3 <= side_1:
-        print("Error: Triangle Inequality Theorem")
     semi_perimeter = 0.5 * (side_1 + side_2 + side_3)
     area_squared = semi_perimeter * (semi_perimeter - side_1) * (semi_perimeter - side_2) * (semi_perimeter - side_3)
     area = area_squared ** (1 / 2)
