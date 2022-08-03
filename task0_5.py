3737cac54cf82b6c220841ab8a1e12abf2f863fc
Phemelo-Motlhabani <skateboardhawkp@gmail.com>
2022-08-03 19:48:40 +0200

    final refactoring

--- task0_5.py
+++ task0_5.py
@@ -1,9 +1,7 @@
 def area_of_triangle(side_1, side_2, side_3):
     semi_perimeter = 0.5 * (side_1 + side_2 + side_3)
     area_squared = semi_perimeter * (semi_perimeter - side_1) * (semi_perimeter - side_2) * (semi_perimeter - side_3)
-    area = area_squared ** (1 / 2)
-    return area
-
+    return area_squared ** (1 / 2)
 
 triangle_area = area_of_triangle(3, 4, 5)
 print(triangle_area)
