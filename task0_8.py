d771b8fe2c40467f9cca3cb5caf17608447ffc89
Phemelo-Motlhabani <skateboardhawkp@gmail.com>
2022-08-04 11:51:43 +0200

    moved variables to the beginning

--- task0_8.py
+++ task0_8.py
@@ -1,28 +1,22 @@
-def time_converter(minutes: int):
+def int_to_time(minutes: int):
+    hours = minutes // 60
+    minutes_converter = minutes - 60
+    hr_minutes_converter = minutes - hours * 60
     if minutes == 0:
-        print(f"0 hours, {minutes} minutes")
+        return f"{hours} hours, {minutes} minutes"
     elif minutes == 1:
-        print(f"0 hours, {minutes} minute")
+        return f"{hours} hours, {minutes} minute"
     elif minutes < 60:
-        print(f"0 hours, {minutes} minutes")
+        return f"{hours} hours, {minutes} minutes"
     elif minutes == 60:
-        print("1 hour, 0 minutes")
+        return f"{hours} hour, {minutes_converter} minutes"
     elif minutes == 61:
-        print("1 hour, 1 minute")
+        return f"{hours} hour, {minutes_converter} minute"
     elif minutes > 60 and minutes < 120:
-        minutes_converter = minutes - 60
-        print(f"1 hour, {minutes_converter} minutes")
+        return f"{hours} hour, {minutes_converter} minutes"
     elif minutes % 60 == 0 and minutes > 60:
-        hours = int(minutes / 60)
-        print(f"{hours} hours, 0 minutes")
-    elif ((minutes % 60) - 1 == 0) and minutes > 60:
-        hours = int((minutes - 1) / 60)
-        print(f"{hours} hours, 1 minute")
+        return f"{hours} hours, {hr_minutes_converter} minutes"
+    elif minutes % 60 == 1 and minutes > 60:
+        return f"{hours} hours, {hr_minutes_converter} minute"
     else:
-        hours = minutes // 60
-        minutes_converter = minutes - (hours * 60)
-        print(f"{hours} hours, {minutes_converter} minutes")
-
-
-time_converter(71)
-time_converter(133)
+        return f"{hours} hours, {hr_minutes_converter} minutes"
