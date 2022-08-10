e4aa25c47184f07fec62c3aa47e8d221ec5d9a4b
Phemelo-Motlhabani <skateboardhawkp@gmail.com>
2022-08-10 13:06:11 +0200

    max number for all integers

--- task0_6.py
+++ task0_6.py
@@ -1,10 +1,10 @@
 def maximum(*numbers):
-    max_number = 0
+    max_number = numbers[0]
     for number in numbers:
         if number > max_number:
             max_number = number
     return max_number
 
 
-max_num = maximum(9, 2, 12, 36, 36, 17, 13, 55, 45, 8, 42, 31, 28, 2, 8)
+max_num = maximum(-2, -33, -1, -9)
 print(max_num)
